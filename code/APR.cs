fac2068f8aec0db321c161fcf33041dc8fc9fcff
dch <>
2023-07-09 00:32:04 -0500

    Add log statement/sound so people get some feedback when clicking the refresh button

--- code/APR.cs
+++ code/APR.cs
@@ -3,5 +3,10 @@
 public static class APR
 {
 	[Menu( "Editor", "AssetPartyRefresher/Refresh", "refresh" )]
-	public static void Refresh() => Utility.ClearPackageCache();
+	public static void Refresh()
+	{
+		Log.Info( "AssetPartyRefresher :: Refreshing cloud games..." );
+		Utility.ClearPackageCache();
+		Utility.PlayRawSound( "sounds/editor/success.wav" );
+	}
 }
